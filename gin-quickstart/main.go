27f40803984a307409c35276a00101c88ba61394
Nikunjjjjjjj <116074466+Nikunjjjjjjj@users.noreply.github.com>
2025-11-08 11:31:07 +0530

    Add files via upload

---
+++ gin-quickstart/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listens on 0.0.0.0:8080 by default
+}
+
+// package main
+
+// import (
+// 	"errors"
+// 	"fmt"
+// )
+
+// func divide(a, b float64) (float64, error) {
+// 	if b == 0 {
+// 		return 0, errors.New("cannot divide by zero")
+// 	}
+// 	return a / b, nil
+// }
+
+// func main() {
+// 	result, err := divide(10, 0)
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		return
+// 	}
+// 	fmt.Println("Result:", result)
+// }
